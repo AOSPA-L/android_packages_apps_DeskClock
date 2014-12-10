8bce5c5c29da728c937c56785481c334d7d19d49
Michael Bestas <mikeioannina@gmail.com>
2014-12-10 22:55:44 +0200

    DeskClock: Make settings consistent
    * Move status bar icon setting to the bottom
    * Match volume button summary with shake/flip actions
    * Match shake/flip actions popup with volume buttons popup setting
    * Make dialog titles consistent
    
    Change-Id: If9000bc843b0feeab40118ee00bd61fbcddf4e9a

--- src/com/android/deskclock/SettingsActivity.java
+++ src/com/android/deskclock/SettingsActivity.java
@@ -151,8 +151,7 @@ public class SettingsActivity extends PreferenceActivity
             notifyHomeTimeZoneChanged();
         } else if (KEY_VOLUME_BUTTONS.equals(pref.getKey())) {
             final ListPreference listPref = (ListPreference) pref;
-            final int idx = listPref.findIndexOfValue((String) newValue);
-            listPref.setSummary(listPref.getEntries()[idx]);
+            updateActionSummary(listPref, (String) newValue, R.string.volume_buttons_summary);
         } else if (KEY_FLIP_ACTION.equals(pref.getKey())) {
             final ListPreference listPref = (ListPreference) pref;
             updateActionSummary(listPref, (String) newValue, R.string.flip_action_summary);
@@ -219,7 +218,7 @@ public class SettingsActivity extends PreferenceActivity
         listPref.setSummary(listPref.getEntry());
 
         listPref = (ListPreference) findPreference(KEY_VOLUME_BUTTONS);
-        listPref.setSummary(listPref.getEntry());
+        updateActionSummary(listPref, listPref.getValue(), R.string.volume_buttons_summary);
         listPref.setOnPreferenceChangeListener(this);
 
         listPref = (ListPreference) findPreference(KEY_FLIP_ACTION);
